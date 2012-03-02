aca99abd480e861a5688313229492f314e4a68c3
TexasGamer <tpgaubert@gmail.com>
2012-03-01 22:31:38 -0600

    Update for latest Bukkit

--- src/main/java/com/texasgamer/russianroulette/Config.java
+++ src/main/java/com/texasgamer/russianroulette/Config.java
@@ -4,21 +4,20 @@ import java.io.File;
 import java.util.List;
 import java.util.logging.Logger;
 
-import org.bukkit.util.config.Configuration;
-
 public class Config 
 {
+	
 	private static RussianRoulette plugin;
-	public static final Logger logger = Logger.getLogger("Minecraft.RussianRoulette");
-	public Config(RussianRoulette instance) 
-	{
+	public static final Logger logger = Logger.getLogger("Minecraft");
+	
+	public Config(RussianRoulette instance) {
 		plugin = instance;
 	}
-
+	
 	public static String directory = "plugins" + File.separator +"RussianRoulette";
 	static File file = new File(directory + File.separator + "config.yml");
 	
-	public void configCheck(){
+	public void configCheck() {
 		new File(directory).mkdir();
 		if(!file.exists()){
 			try {
@@ -30,40 +29,38 @@ public class Config
 		} else {
 		}
 	}
-	private void write(String root, Object x){
-		Configuration config = load();
-		config.setProperty(root, x);
-		config.save();
+	
+	public void addDefaults() {
+		logger.info("Bedtime: Generating config file...");
+		
+		plugin.getConfig().options().copyDefaults(true);
+		
+		String idleTime = "ignore-ops";
+		String idleCommand = "target-across-worlds";
+		
+		plugin.getConfig().addDefault(idleTime, true);
+		plugin.getConfig().addDefault(idleCommand, true);
+		plugin.saveConfig();
 	}
+	
 	public static Boolean readBoolean(String root){
-		Configuration config = load();
-		return config.getBoolean(root, true);
+		return plugin.getConfig().getBoolean(root);
 	}
-	static Double readDouble(String root){
-		Configuration config = load();
-		return config.getDouble(root, 0);
+	
+	public static Double readDouble(String root){
+		return plugin.getConfig().getDouble(root);
 	}
-	private List<String> readStringList(String root){
-		Configuration config = load();
-		return config.getKeys(root);
+	
+	public static int readInt(String root) {
+		return plugin.getConfig().getInt(root);
 	}
+	
+	@SuppressWarnings("unchecked")
+	public static List<String> readList(String root){
+		return (List<String>) plugin.getConfig().getList(root);
+	}
+	
 	public static String readString(String root){
-		Configuration config = load();
-		return config.getString(root);
-	}
-	private static Configuration load(){
-		try {
-			Configuration config = new Configuration(file);
-			config.load();
-			return config;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	private void addDefaults(){
-		logger.info("RussianRoulette: Generating Config File...");
-		write("ignore-ops",true);
-		write("target-across-worlds",true);
+		return plugin.getConfig().getString(root);
 	}
 }
